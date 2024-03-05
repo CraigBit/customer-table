f3e491038b3cd8512065d58615bcc6f259e104c3
CraigBit <listokbymagi@gmail.com>
2024-03-05 21:08:18 +0700

    Компонент поискового фильтра

---
+++ src/app/components/table-search/table-search.component.ts
@@ -0,0 +1,31 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-table-search',
+  templateUrl: './table-search.component.html',
+  styleUrls: ['./table-search.component.scss'],
+  standalone: true,
+  imports: [FormsModule],
+})
+export class TableSearchComponent {
+  /**
+   * Состояние поискового фильтра
+   */
+  @Input() public state = '';
+
+  /**
+   * Событие сортировки
+   */
+  @Output() private searchEvent = new EventEmitter<string | null>();
+
+  /**
+   * Установить поисковой фильтр для таблицы
+   */
+  public setSearch(): void {
+    const trimmedValue = this.state.trim();
+    console.log(this.state);
+
+    this.searchEvent.emit(trimmedValue.length ? trimmedValue : null);
+  }
+}
