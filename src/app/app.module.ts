470065e8fcfa4b6f1629b44c2fa9a5b370c08862
CraigBit <listokbymagi@gmail.com>
2024-03-05 21:09:43 +0700

    Конечная конфигурация app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { TableControlsComponent } from './components/table-controls/table-controls.component';
+import { TableScrollDirective } from './directives/table-scroll.directive';
+import { TableSortComponent } from './components/table-sort/table-sort.component';
+import { TableSearchComponent } from './components/table-search/table-search.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    TableControlsComponent,
+    HttpClientModule,
+    TableScrollDirective,
+    TableSortComponent,
+    FormsModule,
+    ReactiveFormsModule,
+    TableSearchComponent,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
